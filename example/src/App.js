d5b95a7a6607ab00b6e28b4740a72fc27cacc532
thoriqdharmawan <thoriqd21@gmail.com>
2020-06-10 19:10:02 +0700

    penambahan beberapa fitur

--- example/src/App.js
+++ example/src/App.js
@@ -4,9 +4,44 @@ import { Wrapper } from "react-flip";
 import "react-flip/dist/index.css";
 
 const App = () => {
+  const style = {
+    content: {
+      backgroundColor: "#bbb",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      backfaceVisibility: "hidden",
+    },
+    content2: {
+      backgroundColor: "#2980b9",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      backfaceVisibility: "hidden",
+      transform: "rotateY(180deg)",
+    },
+  };
   return (
-    <div style={{height: '100vh', width: '100vw', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-      <Wrapper />
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Wrapper
+        trigger="click"
+        // flipForward
+        rotation="left"
+        duration={0.6}
+        flipCount={1}
+        perspective="900px"
+      >
+        <div style={style.content}></div>
+        <div style={style.content2}></div>
+      </Wrapper>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
 
-export const Wrapper = () => {
+export const Wrapper = (props) => {
+  const {
+    flipForward = false,
+    rotation = "right",
+    duration = 0.6,
+    flipCount = 1,
+    perspective = '500px',
+    trigger = "hover",
+    ...child
+  } = props;
   const [hover, setHover] = useState(false);
-  const [deg, setDeg] = useState(180);
+  const [oddClick, setOddClick] = useState(true);
+  const [deg, setDeg] = useState(0);
 
   const style = {
     normal: {
       backgroundColor: "transparent",
-      width: 100,
-      height: 100,
-      perspective: "1000px",
+      width: 400,
+      height: 400,
+      perspective: perspective,
     },
     inner: {
       position: "relative",
       width: "100%",
       height: "100%",
-      transition: "transform 0.6s",
+      transition: `transform ${duration}s`,
       boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
       transformStyle: "preserve-3d",
     },
@@ -39,26 +49,87 @@ export const Wrapper = () => {
     },
   };
 
+  const rotationStart = () => {
+    let rotationDeg = 0;
+
+    if (rotation === "left") {
+      rotationDeg = deg - (180 * flipCount);
+    } else {
+      rotationDeg = deg + (180 * flipCount);
+    }
+    setDeg(rotationDeg);
+  }
+
+  const rotationEnd = () => {
+    let rotationDeg = 0;
+    if (rotation === "left") {
+      if (flipForward) {
+        rotationDeg = deg - (180 * flipCount);
+      } else {
+        rotationDeg = deg + (180 * flipCount);
+      }
+    } else {
+      if (flipForward) {
+        rotationDeg = deg + (180 * flipCount);
+      } else {
+        rotationDeg = deg - (180 * flipCount);
+      }
+    }
+    setDeg(rotationDeg);
+  }
+  
+  const rotationClick = () => {
+    let rotationDeg = 0;
+    if(flipForward) {
+      if(rotation === "left") {
+        rotationDeg = deg - 180
+      } else {
+        rotationDeg = deg + 180
+      }
+    } else {
+      if(rotation === "left") {
+        if(oddClick) {
+          rotationDeg = deg - 180
+        } else {
+          rotationDeg = deg - 180
+        }
+      } else {
+        if(oddClick) {
+          rotationDeg = deg + 180
+        } else {
+          rotationDeg = deg + 180
+        }
+      }
+    }
+    setDeg(rotationDeg);
+  }
+
   const onMouseEnter = () => {
-    setHover(true);
-  }
-  
+    rotationStart()
+  };
+
   const onMouseLeave = () => {
-    setHover(false);
-  }
+    rotationEnd()
+  };
   
+  const rotationOnClick = () => {
+    setOddClick(!oddClick)
+    rotationClick()
+  };
+  
+
   return (
     <div
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
+      onMouseEnter={trigger === "hover" && onMouseEnter}
+      onMouseLeave={trigger === "hover" && onMouseLeave}
+      onClick={trigger === "click" && rotationOnClick}
       style={{
         ...style.normal,
       }}
     >
-      <div style={{ ...style.inner, ...(hover ? style.hover : null) }}>
-        <div style={style.content}></div>
-        <div style={style.content2}></div>
-      </div>
+      <div style={{ ...style.inner, ...style.hover }} {...child} />
+      {/* <div style={style.content}></div>
+        <div style={style.content2}></div> */}
     </div>
   );
 };
